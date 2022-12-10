6038d589111f11937676e3b2b8f28c6d13064cb5
Victor Perdiguer <victor.perdiguer@gmail.com>
2022-12-10 14:46:53 +0100

    express links done, missing actual content

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    console.log(req);
+    res.status(200).sendFile(__dirname + '/views/index.html');
+})
+
+app.get('/about', (req, res) => {
+    console.log(req);
+    res.status(200).sendFile(__dirname + '/views/about.html');
+})
+
+app.get('/works', (req, res) => {
+    console.log(req);
+    res.status(200).sendFile(__dirname + '/views/works.html');
+})
+
+app.get('/photo-gallery', (req, res) => {
+    console.log(req);
+    res.status(200).sendFile(__dirname + '/views/photogallery.html');
+})
+
+
+
+
+
+
+app.listen(3000, () => { console.log('App running on port 3000') });
