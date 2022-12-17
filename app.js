b99ea2124f19d7ddbcdc2611e4ff9ac655171c80
Victor Perdiguer <victor.perdiguer@gmail.com>
2022-12-17 11:00:12 +0100

    WIP gallery

--- app.js
+++ app.js
@@ -18,14 +18,9 @@ app.get('/works', (req, res) => {
     res.status(200).sendFile(__dirname + '/views/works.html');
 })
 
-app.get('/photo-gallery', (req, res) => {
+app.get('/gallery', (req, res) => {
     console.log(req);
     res.status(200).sendFile(__dirname + '/views/photogallery.html');
 })
-
-
-
-
-
 
 app.listen(3000, () => { console.log('App running on port 3000') });
